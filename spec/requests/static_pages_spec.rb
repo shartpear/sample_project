e6995726c801c9e9014e89299111ef37595e5942
shartpear <william@labs.epiuse.com>
2012-05-16 14:44:33 -0400

    adds CSS framework

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "StaticPages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "#{base_title}")
     end
     it "should not have a custom page title" do
       visit '/static_pages/home'
