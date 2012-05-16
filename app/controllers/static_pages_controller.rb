e96d0ca574fd4c8305939dde5f6dd771ff354dd9
shartpear <william@labs.epiuse.com>
2012-05-16 09:55:58 -0400

    Adds static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+  def about
+    
+  end
+  
   def home
   end
 
   def help
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleProject::Application.routes.draw do
   get "static_pages/home"
 
+  get "static_pages/about"
+  
   get "static_pages/help"
 
   # The priority is based upon order of creation:
