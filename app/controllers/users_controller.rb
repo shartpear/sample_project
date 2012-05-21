a8d2ac6fad74238ac4cb50c307496c4562919a3c
shartpear <william@labs.epiuse.com>
2012-05-21 10:46:29 -0400

    completes friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only:[:edit, :update]
+  
   def new
     @user=User.new
   end
@@ -15,4 +18,42 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+   # @user = User.find(params[:id])    #moved to correct_user before filter
+    if @user.update_attributes(params[:user])
+      flash[:success]="Profile Updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+    
+  end#end of edit
+  
+  def update
+   # @user = User.find(params[:id])   #moved to correct_user before filter
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end #end of update
+  
+  
+  private
+  def signed_in_user
+    unless signed_in?
+      store_location
+    
+    redirect_to signin_path, notice: "Please sign in." 
+    end
+  end
+  
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+  
 end
