14a191141e27eb38eb639070a966df16d55f4453
shartpear <william@labs.epiuse.com>
2012-05-21 15:52:53 -0400

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :signed_in_user
+   before_filter :correct_user,   only: :destroy
 
   def create
     @micropost=current_user.microposts.build(params[:micropost])
@@ -12,6 +13,27 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy
+   def destroy
+     @user=@micropost.user
+    @micropost.destroy
+    if (current_user.admin?)
+      redirect_to @user
+    else
+    redirect_to root_path
   end
+  end
+
+  private
+
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      if @micropost.nil?
+        if (current_user.admin?)
+          @micropost =Micropost.find_by_id(params[:id])
+        else
+          redirect_to root_path 
+        end
+      end
+      
+    end
 end
